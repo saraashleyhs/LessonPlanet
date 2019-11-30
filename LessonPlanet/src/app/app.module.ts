d4a88d167eebed1cb6d5db724e8081b84df30296
saraashleyhs <sara.ashleyhs@gmail.com>
2019-11-29 18:39:32 -0600

    Initial Setup

---
+++ LessonPlanet/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ModalModule.forRoot(),  
+    NgbModule  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
