6ca63aad769d2f7ee07b3324020edfe6d474f8c7
saraashleyhs <sara.ashleyhs@gmail.com>
2019-11-29 18:46:54 -0600

    Added Components

--- LessonPlanet/src/app/app.module.ts
+++ LessonPlanet/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { HomeComponent } from './home/home.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { DayLessonsComponent } from './calendar/day-lessons/day-lessons.component';
+import { LessonsComponent } from './lessons/lessons.component';
+import { TasksComponent } from './lessons/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CalendarComponent,
+    DayLessonsComponent,
+    LessonsComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
---
+++ LessonPlanet/src/app/calendar/day-lessons/day-lessons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day-lessons',
+  templateUrl: './day-lessons.component.html',
+  styleUrls: ['./day-lessons.component.css']
+})
+export class DayLessonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
