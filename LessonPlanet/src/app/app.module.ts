bb1a72685af338f5be9e9f269d74d86e3fad7c31
saraashleyhs <sara.ashleyhs@gmail.com>
2019-12-06 20:40:06 -0600

    adding dependencies for calendar

--- LessonPlanet/src/app/app.module.ts
+++ LessonPlanet/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { LessonsComponent } from './lessons/lessons.component';
 import { TasksComponent } from './lessons/tasks/tasks.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   declarations: [
@@ -27,7 +30,12 @@ import { LoginComponent } from './login/login.component';
     AppRoutingModule,
     ModalModule.forRoot(),  
     NgbModule,
-    HttpClientModule 
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }) 
   ],
   providers: [],
   bootstrap: [AppComponent]
